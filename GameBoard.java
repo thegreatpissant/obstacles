f42443a8f01d02ddd9a42b99c09d889a774e82bf
JAF <openjaf@gmail.com>
2020-09-12 23:49:47 -0400

    Updates for Version 2
    - Add Scaling of the board to window size
    - Add other static objects to board
    - Add draw functions for other objects
    - Remove redundant draw code
    - Add two more example boards
    - Track the current playing board index
    - Add a beat the board check
    - Add a beat the game check

--- GameBoard.java
+++ GameBoard.java
@@ -7,8 +7,11 @@
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 /**
  * Direction the player is moving
@@ -20,17 +23,23 @@ enum Direction {
 /**
  * The Game Board that represents the maze.
  */
-public class GameBoard extends JPanel implements KeyListener {
+public class GameBoard extends JPanel implements KeyListener, ComponentListener {
 
     //  Tokens
-    static final char PLAYER_TOKEN = 'x';
-    static final char END_TOKEN = '*';
-    static final char OPEN_TOKEN = '.';
-    static final char OBSTACLE_TOKEN = 'o';
+    final char PLAYER_TOKEN = 'x';
+    final char ENDGOAL_TOKEN = '*';
+    final char OPEN_TOKEN = '.';
+    final char OBSTACLE_TOKEN = 'o';
+    final char HAZARD_TOKEN = 'v';
+    final char RESET_PLAYER_TOKEN = 'r';
 
     //  GameBoard Cell Dimensions
-    static int cellHeight;
-    static int cellWidth;
+    int cellHeight;
+    int cellWidth;
+
+    //  GameBoard Pixel Dimensions
+    int cellPixelWidth;
+    int cellPixelHeight;
 
     //  Board Width in cells
     static int boardWidth;
@@ -42,18 +51,50 @@ public class GameBoard extends JPanel implements KeyListener {
     //  Player Sprite
     //  @@TODO: add player sprite
 
+    //  Start position
+    Point startPosition = new Point();
     //  Goal position
     Point goalPosition = new Point();
     //  Goal Sprite
     //  @@TODO: add goal sprite
 
+    //  Locations of hazards
+    ArrayList<Point> hazardPositions = new ArrayList<>();
+    //  Locations of obstacles
+    ArrayList<Point> obstaclePositions = new ArrayList<>();
+    //  Location of reset Positions
+    ArrayList<Point> resetPositions = new ArrayList<>();
 
     //  Board
     String[] board;
 
-    String[] defaultBoard = {
+    String[] defaultBoardOne = {
+            "*..........",
             "...........",
             "...........",
+            "...........",
+            "...o.......",
+            "...........",
+            "...........",
+            "...........",
+            "...........",
+            "..........x",
+    };
+    String[] defaultBoardTwo = {
+            "..........x",
+            "...........",
+            "...r.......",
+            "...........",
+            "...........",
+            "...........",
+            "...........",
+            "...........",
+            "...........",
+            "*..........",
+    };
+    String[] defaultBoardThree = {
+            "...........",
+            "........v..",
             "...........",
             "...........",
             "....x......",
@@ -63,154 +104,189 @@ public class GameBoard extends JPanel implements KeyListener {
             "...........",
             "..........*",
     };
+    String[][] boards = {defaultBoardOne, defaultBoardTwo, defaultBoardThree};
 
-    //  Render Panel
+    //  Current board the player is on.
+    int currentBoardIndex;
+
+    //  Temp player position, work on this player position before committing.
+    private Point tempPlayerPosition;
 
     //  Constructor
     public GameBoard() {
-        loadGameBoard(defaultBoard);
+        //  To handle resize events.
+        this.addComponentListener(this);
+
+        //  Reset the game to initial state
+        this.resetGame();
+
+        //  Load the game.
+        this.loadGameBoard();
     }
 
+    private void resetGame() {
+        //  Set the default board
+        this.currentBoardIndex = 0;
+    }
     //  Load Game Board
-    private void loadGameBoard(String[] gameBoard) {
+    private void loadGameBoard() {
+        if (this.currentBoardIndex >= this.boards.length) {
+            System.out.println("Board #" + this.currentBoardIndex + " is invalid, maxboards is: " + this.boards.length);
+            this.currentBoardIndex = this.boards.length - 1;
+        }
+        String[] gameBoard = this.boards[this.currentBoardIndex];
         //  Set the board as the passed in gameBoard
         this.board = gameBoard;
 
         //  Set the board Dimensions in cells
-        boardHeight = gameBoard.length;
-        boardWidth = gameBoard[0].length();
+        this.boardHeight = gameBoard.length;
+        this.boardWidth = gameBoard[0].length();
 
-        //  Scale the cell dimensions
-        cellHeight = Obstacles.APP_HEIGHT / (GameBoard.boardHeight+1);
-        cellWidth = Obstacles.APP_WIDTH / (GameBoard.boardWidth+1);
+        //  Set the cell size
+        this.updateCellSize();
+
+        //  Clear any static objects
+        hazardPositions.clear();
+        obstaclePositions.clear();
+        resetPositions.clear();
 
         //  Scan the board
-        Point tempPlayerPosition = new Point();
-        Point tempGoalPosition = new Point();
+        Point scannedPlayerPosition = new Point();
+        Point scannedGoalPosition = new Point();
         boolean playerPositionSet = false;
         boolean goalPositionSet = false;
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[y].length(); x++) {
                 switch (board[y].charAt(x)) {
                     case PLAYER_TOKEN:
-                            tempPlayerPosition = new Point(x, y);
+                            scannedPlayerPosition = new Point(x, y);
                             playerPositionSet = true;
                         break;
-                    case END_TOKEN:
-                            tempGoalPosition = new Point(x, y);
+                    case ENDGOAL_TOKEN:
+                            scannedGoalPosition = new Point(x, y);
                             goalPositionSet = true;
                         break;
                     case OBSTACLE_TOKEN:
+                        this.obstaclePositions.add(new Point(x, y));
+                        break;
+                    case RESET_PLAYER_TOKEN:
+                        this.resetPositions.add(new Point(x, y));
+                        break;
+                    case HAZARD_TOKEN:
+                        this.hazardPositions.add(new Point(x, y));
+                        break;
+                    case OPEN_TOKEN:
+                        break;
                     default:
-                            break;
-
+                        System.out.println("Map Token type: \"" + board[y].charAt(x) + "\" not recognized.");
+                        break;
                 }
             }
         }
         if (!playerPositionSet) {
             System.out.println("MAP Player position not found");
         }
-        this.playerPosition = tempPlayerPosition;
+        this.playerPosition = scannedPlayerPosition;
+        this.startPosition = scannedPlayerPosition;
         if (!goalPositionSet) {
             System.out.println("MAP Goal position not found");
         }
-        this.goalPosition = tempGoalPosition;
+        this.goalPosition = scannedGoalPosition;
+    }
+
+    //  Draw a tile
+    private void drawTile(Graphics graphics, Point point) {
+        graphics.fillRect(point.x * cellWidth,point.y * cellHeight,cellWidth,cellHeight );
     }
 
     //  Draw the player
     private void drawPlayer(Graphics graphics) {
         graphics.setColor(Color.black);
-        graphics.fillRect(
-                (int)(this.playerPosition.x * cellWidth * Obstacles.BOARD_SCALE),
-                (int)(this.playerPosition.y * cellHeight * Obstacles.BOARD_SCALE),
-                (int)(cellWidth * Obstacles.BOARD_SCALE),
-                (int)(cellHeight * Obstacles.BOARD_SCALE));
-
-//        BufferedImage img = null;
-//        try {
-//            img = ImageIO.read(new File("Cylinder.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        graphics.drawImage(img,
-//                (int)(this.playerPosition.x * Obstacles.BOARD_SCALE),
-//                (int)(this.playerPosition.y * Obstacles.BOARD_SCALE),
-//                (int)(cellDimension * Obstacles.BOARD_SCALE),
-//                (int)(cellDimension * Obstacles.BOARD_SCALE),
-//                Color.LIGHT_GRAY, null);
+        this.drawTile(graphics, this.playerPosition);
     }
 
     //  Draw the player
     private void drawGoal(Graphics graphics) {
         graphics.setColor(Color.ORANGE);
-        graphics.fillRect(
-                (int)(this.goalPosition.x * cellWidth * Obstacles.BOARD_SCALE),
-                (int)(this.goalPosition.y * cellHeight * Obstacles.BOARD_SCALE),
-                (int)(cellWidth * Obstacles.BOARD_SCALE),
-                (int)(cellHeight * Obstacles.BOARD_SCALE));
-
-//        BufferedImage img = null;
-//        try {
-//            img = ImageIO.read(new File("Cylinder.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        graphics.drawImage(img,
-//                (int)(this.playerPosition.x * Obstacles.BOARD_SCALE),
-//                (int)(this.playerPosition.y * Obstacles.BOARD_SCALE),
-//                (int)(cellDimension * Obstacles.BOARD_SCALE),
-//                (int)(cellDimension * Obstacles.BOARD_SCALE),
-//                Color.LIGHT_GRAY, null);
+        this.drawTile(graphics, this.goalPosition);
     }
 
+    //  Draw the player
+    private void drawHazards(Graphics graphics) {
+        graphics.setColor(Color.RED);
+        for ( Point point : this.hazardPositions) {
+            this.drawTile(graphics, point);
+        }
+    }
 
+    //  Draw the player
+    private void drawResets(Graphics graphics) {
+        graphics.setColor(Color.GREEN);
+        for ( Point point : this.resetPositions) {
+            this.drawTile(graphics, point);
+        }
+    }
 
+    //  Draw the player
+    private void drawObstacles(Graphics graphics) {
+        graphics.setColor(Color.BLUE);
+        for ( Point point : this.obstaclePositions) {
+            this.drawTile(graphics, point);
+        }
+    }
     //  Draw the world
     public void draw(Graphics graphics) {
         //  Draw the board
         //  @@TODO Draw all the things.
 
-        //  Draw the goal, should be one of the things.
+        //  Draw the static map objects
         this.drawGoal(graphics);
+        this.drawObstacles(graphics);
+        this.drawHazards(graphics);
+        this.drawResets(graphics);
 
         //  Draw the player
         this.drawPlayer(graphics);
     }
 
+    //  Update the size of a cell
+    private void updateCellSize() {
+        //  Scale the cell dimensions
+        cellHeight = this.getHeight() / (GameBoard.boardHeight+1);
+        cellWidth = this.getWidth() / (GameBoard.boardWidth+1);
+    }
+
     //  Move player
     public void movePlayer(Direction direction) {
         //  Work with a temp version of the player position.
-        Point tempPlayerPosition = new Point(playerPosition);
-        switch(direction) {
+        this.tempPlayerPosition = new Point(playerPosition);
+        switch (direction) {
             case RIGHT:
-                tempPlayerPosition.x += 1;
+                this.tempPlayerPosition.x += 1;
                 break;
             case LEFT:
-                tempPlayerPosition.x -= 1;
+                this.tempPlayerPosition.x -= 1;
                 break;
             case UP:
-                tempPlayerPosition.y -= 1;
+                this.tempPlayerPosition.y -= 1;
                 break;
             case DOWN:
-                tempPlayerPosition.y += 1;
+                this.tempPlayerPosition.y += 1;
                 break;
         }
-
+    }
+    private void checkPlayerCollisions() {
         //  Is the player in board bounds?
-        if (tempPlayerPosition.x >= 0 && tempPlayerPosition.x <= boardWidth &&
-                tempPlayerPosition.y >= 0 && tempPlayerPosition.y <= boardHeight) {
+        if (this.tempPlayerPosition.x >= 0 && this.tempPlayerPosition.x <= boardWidth &&
+                this.tempPlayerPosition.y >= 0 && this.tempPlayerPosition.y <= boardHeight) {
 
             //  Did the player collide with an obstacle?
             //  - Update position
-            //  - Update Health
+            //  - ??Update Health??
             //  - Teleport the player
 
             //  Update the player position.
-            playerPosition = tempPlayerPosition;
-
-            if (playerPosition == this.goalPosition) {
-                JOptionPane.showInputDialog(null,"You have Won!", null);
-            }
+            this.playerPosition = this.tempPlayerPosition;
         }
     }
 
@@ -222,11 +298,6 @@ public class GameBoard extends JPanel implements KeyListener {
         graphics.setColor(Color.PINK);
 
         this.draw(graphics);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent keyEvent) {
-
     }
 
     @Override
@@ -256,11 +327,59 @@ public class GameBoard extends JPanel implements KeyListener {
                 System.out.println("KeyPress = " + keyEvent.getKeyChar());
                 break;
         }
+
+        //  Check player collisions
+        this.checkPlayerCollisions();
+
+        //  repaint board
+        this.repaint();
+
+        //  Check player win condition
+        if (beatLevel()) {
+            //  Yes, Increment to the next board.
+            this.currentBoardIndex++;
+
+            //  Did they also beat the game?
+            if (this.beatGame()) {
+                //  Yes, tell them they won and set the next board to 0
+                JOptionPane.showMessageDialog(this,"You beat the Game!!\n Restarting at level 1");
+                this.resetGame();
+                this.loadGameBoard();
+            }
+            else {
+                JOptionPane.showMessageDialog(this,"You beat the board!!");
+                //  No, load the next board.
+                this.loadGameBoard();
+            }
+        }
+        this.repaint();
+    }
+
+    private boolean beatGame() {
+        return this.currentBoardIndex >= this.boards.length;
+    }
+
+    private boolean beatLevel() {
+        return this.playerPosition.equals(this.goalPosition);
+    }
+    @Override
+    public void componentResized(ComponentEvent componentEvent) {
+        this.updateCellSize();
         this.repaint();
     }
 
     @Override
-    public void keyReleased(KeyEvent keyEvent) {
+    public void keyTyped(KeyEvent keyEvent) { }
 
-    }
+    @Override
+    public void keyReleased(KeyEvent keyEvent) { }
+
+    @Override
+    public void componentMoved(ComponentEvent componentEvent) { }
+
+    @Override
+    public void componentShown(ComponentEvent componentEvent) { }
+
+    @Override
+    public void componentHidden(ComponentEvent componentEvent) { }
 }
